a545a391213bd043002916d5fb188a64b502e074
dlereya <DLereya@gmail.com>
2012-03-24 08:04:36 +0000

    First Commit - empty basic project folders

---
+++ picabus/src/com/zdm/picabus/PicabusActivity.java
@@ -0,0 +1,15 @@
+package com.zdm.picabus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class PicabusActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Toast.makeText(getApplicationContext(), "ss", 3);
+    }
+}

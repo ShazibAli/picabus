9fe433f7b90b3fcc31a7fae0d934f5755d1c6080
dlereya@gmail.com <dlereya@gmail.com@e7f404d5-e9fd-1835-0f93-09655e65c390>
2012-04-10 21:40:10 +0000

    1. refactoring 'IniConfigs', ConnectionPool 2. Adding customException for DB ini configurations 3. Adding the client library 4. Location handler - first step

--- picabus/src/com/zdm/picabus/db/framework/IniConfigs.java
+++ picabus/src/com/zdm/picabus/db/framework/IniConfigs.java
@@ -1,13 +1,18 @@
 package com.zdm.picabus.db.framework;
 
-
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-
+/**
+ * DB ini file configurations parser.
+ * 
+ * @author Daniel Lereya
+ * 
+ */
 public class IniConfigs {
 
 	private String hostName;
@@ -15,96 +20,118 @@ public class IniConfigs {
 	private String schemaName;
 	private String username;
 	private String password;
-	private int maxNumConnections;
-	private int initNumConnections;
-	
-	public IniConfigs(String iniFile){
-	
-		
-		FileInputStream fis;
-		try {
-			fis = new FileInputStream(iniFile);
-			DataInputStream dis = new DataInputStream(fis);
-			
-			BufferedReader file = new BufferedReader(new InputStreamReader(dis));
-			
-			String line;
-			int separator;
-			String variable;
-			String value;
-			
-			this.hostName = null;
-			this.initNumConnections = -1;
-			this.maxNumConnections = -1;
-			this.password = null;
-			this.portNumber = null;
-			this.schemaName = null;
-			this.username = null;
-			
-			
-			while ((line= file.readLine()) != null){
-				separator = line.indexOf('=');
-				variable = line.substring(0, separator).trim();
-				value = line.substring(separator + 1).trim();
-				
-				if (variable.equalsIgnoreCase("hostname")) {
-					this.hostName = value;
-				}
-				else if (variable.equalsIgnoreCase("portnumber")) {
-					this.portNumber = value;
-				}
-				else if (variable.equalsIgnoreCase("schemaname")) {
-					this.schemaName = value;
-				}
-				else if (variable.equalsIgnoreCase("username")) {
-					this.username = value;
-				}
-				else if (variable.equalsIgnoreCase("password")) {
-					this.password = value;
-				}				
-				else if (variable.equalsIgnoreCase("maxnumconnections")) {
-					this.maxNumConnections = Integer.valueOf(value);
-				}
-				else if (variable.equalsIgnoreCase("initnumconnections")) {
-					this.initNumConnections = Integer.valueOf(value);
-				}	
-				else continue;
+	private Integer maxNumConnections;
+	private Integer initNumConnections;
 
-				
-			}
-			
-			dis.close();
-			fis.close();
-			file.close();
-			
-			if (this.hostName == null){
-				throw new Exception("Ini file host name error"); 
-			}
-			if(this.initNumConnections == -1){
-				throw new Exception("Ini file initial number of connection error");
-			}
-			if(this.maxNumConnections == -1){
-				throw new Exception("Ini file maxinum number of connection error");
-			}
-			if(this.password == null){
-				throw new Exception("Ini file passwod error");
-			}
-			if (this.portNumber == null){
-				throw new Exception("Ini file port number error");
-			}
-			if(this.schemaName == null){
-				throw new Exception("Ini file scheme name error");
-			}
-			if(this.username == null){
-				throw new Exception("Ini file username error");
-			}
-			
-		} catch (Exception e) {
-			//TODO: print error message to client layer
-			System.out.println(e.toString());
-		}
-		
+	private final String iniFilePath;
+
+	private FileInputStream fis = null;
+	private DataInputStream dis = null;
+	private BufferedReader file = null;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param iniFilePath
+	 *            full path to the 'config.ini' file.
+	 * 
+	 */
+	public IniConfigs(String iniFilePath) {
+		this.iniFilePath = iniFilePath;
 	}
+
+	/**
+	 * Parse the 'config.ini' file.
+	 * 
+	 * after this method is done, all the relevant configurations is accessible
+	 * through the 'IniConfigs' getters.
+	 * 
+	 * @throws FileNotFoundException
+	 *             'config.ini' file couldn't be found.
+	 * @throws IllegalDBConfigurationsException
+	 *             one or more configurations in the 'config.ini' file is
+	 *             invalid.
+	 * @throws IOException
+	 *             problem occurred while reading the file.
+	 * */
+	public void parseIniFile() throws FileNotFoundException,
+			IllegalDBConfigurationsException, IOException {
+		fis = new FileInputStream(iniFilePath);
+		dis = new DataInputStream(fis);
+		file = new BufferedReader(new InputStreamReader(dis));
+
+		String line;
+		int separator;
+		String configName;
+		String configValue;
+
+		this.hostName = null;
+		this.initNumConnections = -1;
+		this.maxNumConnections = -1;
+		this.password = null;
+		this.portNumber = null;
+		this.schemaName = null;
+		this.username = null;
+
+		ConfigurationErrors ce = new ConfigurationErrors();
+
+		try {
+			while ((line = file.readLine()) != null) {
+				separator = line.indexOf('=');
+				configName = line.substring(0, separator).trim();
+				configValue = line.substring(separator + 1).trim();
+
+				if (configName.equalsIgnoreCase("hostname")) {
+					this.hostName = configValue;
+				} else if (configName.equalsIgnoreCase("portnumber")) {
+					this.portNumber = configValue;
+				} else if (configName.equalsIgnoreCase("schemaname")) {
+					this.schemaName = configValue;
+				} else if (configName.equalsIgnoreCase("username")) {
+					this.username = configValue;
+				} else if (configName.equalsIgnoreCase("password")) {
+					this.password = configValue;
+				} else if (configName.equalsIgnoreCase("maxnumconnections")) {
+					this.maxNumConnections = Integer.valueOf(configValue);
+				} else if (configName.equalsIgnoreCase("initnumconnections")) {
+					this.initNumConnections = Integer.valueOf(configValue);
+				} else
+					continue; // ignoring irrelevant configurations names
+
+			}
+		} catch (NumberFormatException e) {
+			// here, we're just printing the stack trace. later, an 'IllegalDBConfigurationsException' exception will be thrown
+			e.printStackTrace();
+		}
+
+		if (this.hostName == null) {
+			ce.addEror("hostName", null);
+		}
+		if (this.initNumConnections <= 1) {
+			ce.addEror("initNumConnections", this.initNumConnections.toString());
+		}
+		if (this.maxNumConnections <= 1) {
+			ce.addEror("maxNumConnections", this.maxNumConnections.toString());
+		}
+		if (this.password == null) {
+			ce.addEror("password", null);
+		}
+		if (this.portNumber == null) {
+			ce.addEror("protNumber", null);
+		}
+		if (this.schemaName == null) {
+			ce.addEror("schemaName", null);
+		}
+		if (this.username == null) {
+			ce.addEror("username", null);
+		}
+
+		if (ce.isErrorOccured()) {
+			throw new IllegalDBConfigurationsException(
+					"The 'config.ini' file is invalid", ce.getErrorsString());
+		}
+	}
+
 
 	public String getHostName() {
 		return hostName;
@@ -133,7 +160,46 @@ public class IniConfigs {
 	public int getInitNumConnections() {
 		return initNumConnections;
 	}
-	
-	
-	
+
+	/**
+	 * 
+	 * @author Daniel Lereya
+	 *
+	 * Used to generate a descriptive String containing the errors that occurred during 'config.ini' parsing.
+	 * 
+	 */
+	private class ConfigurationErrors {
+		StringBuilder sb = null;
+		boolean confErrOccured;
+
+		public ConfigurationErrors() {
+			this.confErrOccured = false;
+		}
+
+		public void addEror(String configName, String configValue) {
+			String configStrValue = (configValue == null ? "null" : configValue);
+
+			if (sb == null) {
+				sb = new StringBuilder();
+			}
+
+			if (sb.length() == 0) {
+				sb.append(configName + ": " + configStrValue);
+			} else {
+				sb.append(", " + configName + ": " + configStrValue);
+			}
+
+			this.confErrOccured = true;
+		}
+
+		public String getErrorsString() {
+			return sb.toString();
+		}
+
+		public boolean isErrorOccured() {
+			return this.confErrOccured;
+		}
+
+	}
+
 }

1d04be9108f322422e504b71d52d371170d3f1d4
dlereya@gmail.com <dlereya@gmail.com@e7f404d5-e9fd-1835-0f93-09655e65c390>
2012-08-19 17:10:39 +0000

    Adding animation to splash screen

--- picabus/src/com/zdm/picabus/logic/SplashActivity.java
+++ picabus/src/com/zdm/picabus/logic/SplashActivity.java
@@ -2,33 +2,58 @@ package com.zdm.picabus.logic;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.zdm.picabus.R;
-
-public class SplashActivity extends Activity {
-   private static final int splashDurationMS = 1500;
-	
+ 
+public class SplashActivity extends Activity implements AnimationListener {
+     public void onAttachedToWindow() {
+            super.onAttachedToWindow();
+            Window window = getWindow();
+            window.setFormat(PixelFormat.RGBA_8888);
+        }
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-        
-        // Setting the splash
-        Thread timer = new Thread() {
-        	public void run(){
-        		try {
-					sleep(splashDurationMS);
-					Intent openMainScreen = new Intent("com.zdm.picabus.MAINSCREEN");
-        			startActivity(openMainScreen);
-        			finish();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				} finally {
-					
-				}
-        	}
-        };
-        timer.start();
+        StartAnimations();
     }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.setAnimationListener(this);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+ 
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+ 
+    }
+	public void onAnimationEnd(Animation animation) {
+		Intent openMainScreen = new Intent("com.zdm.picabus.MAINSCREEN");
+		startActivity(openMainScreen);
+		finish();
+		
+	}
+	public void onAnimationRepeat(Animation animation) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void onAnimationStart(Animation animation) {
+		// TODO Auto-generated method stub
+		
+	}
+ 
 }

4a03888f88cbe6c108170a80538644eda0280ba8
dlereya@gmail.com <dlereya@gmail.com@e7f404d5-e9fd-1835-0f93-09655e65c390>
2012-08-21 21:31:05 +0000

    Adding reports servlet

---
+++ picabusserver/src/com/zdm/picabus/server/ReportsServlet.java
@@ -0,0 +1,25 @@
+package com.zdm.picabus.server;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ReportsServlet extends HttpServlet {
+
+
+	private static final long serialVersionUID = 1L;
+
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		
+	}
+	
+}

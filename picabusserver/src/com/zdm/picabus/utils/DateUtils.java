02e2ff229fb7078b4a47f4b7b43c714eb745bcd2
dlereya <DLereya@gmail.com>
2012-08-21 21:06:48 +0000

    Date utilities

---
+++ picabusserver/src/com/zdm/picabus/utils/DateUtils.java
@@ -0,0 +1,42 @@
+package com.zdm.picabus.utils;
+
+import java.util.Calendar;
+
+/**
+ * 
+ * Class that provides utilities for working with dates  
+ *
+ */
+public class DateUtils {
+	public static String getDayString(int dayCode) {
+		if (dayCode == java.util.Calendar.SUNDAY) {
+			return "sunday";
+		}
+		else if (dayCode == java.util.Calendar.MONDAY) {
+			return "monday";
+		}
+		else if (dayCode == java.util.Calendar.TUESDAY) {
+			return "tuesday";
+		}
+		else if (dayCode == java.util.Calendar.WEDNESDAY) {
+			return "wednesday";
+		}
+		else if (dayCode == java.util.Calendar.THURSDAY) {
+			return "thursday";
+		}
+		else if (dayCode == java.util.Calendar.FRIDAY) {
+			return "friday";
+		}
+		else if (dayCode == java.util.Calendar.SATURDAY) {
+			return "saturday";
+		}
+		return null;
+	}
+	
+	public static int getCurrentDayCode() {
+		Calendar c = Calendar.getInstance();
+		c.setTime(c.getTime());
+		return c.get(Calendar.DAY_OF_WEEK); 
+	}
+
+}

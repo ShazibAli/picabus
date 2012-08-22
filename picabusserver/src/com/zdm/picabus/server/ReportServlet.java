3b7bbfcd37fd059fc13a23cf61131cd245ff839b
dlereya <DLereya@gmail.com>
2012-08-22 17:41:32 +0000

    Changing servlet time

--- picabusserver/src/com/zdm/picabus/server/ReportServlet.java
+++ picabusserver/src/com/zdm/picabus/server/ReportServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ReportsServlet extends HttpServlet {
+public class ReportServlet extends HttpServlet {
 
 
 	private static final long serialVersionUID = 1L;

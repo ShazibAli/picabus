3e4f0c68caff97ed43454183f4794c6d75197559
dlereya <DLereya@gmail.com>
2012-08-22 17:42:27 +0000

    Supporting Crowd Sourcing - DB Layer

--- picabusserver/src/com/zdm/picabus/db/IDBServices.java
+++ picabusserver/src/com/zdm/picabus/db/IDBServices.java
@@ -9,27 +9,31 @@ import com.zdm.picabus.server.exceptions.EmptyResultException;
 public interface IDBServices {
 
 	/**
+	 * Return the trips with all the required information.
 	 * 
 	 * @param lineNumber
-	 *            the line number 
+	 *            the line number
 	 * @param latitude
 	 *            the latitude value of the bus station
 	 * @param longitude
 	 *            the longitude value of the bus station
 	 * @param clientTime
-	 *            the time of the client (can differ from the server time) 
+	 *            the time of the client (can differ from the server time)
 	 * @param timeIntervalInMinutes
-	 *            specifying the interval in minutes from the client time in which the user wants to see the results
-	 *
+	 *            specifying the interval in minutes from the client time in
+	 *            which the user wants to see the results
+	 * 
 	 * @return bus line containing the relevant data
-	 * @throws EmptyResultException thrown when no result was found
+	 * @throws EmptyResultException
+	 *             thrown when no result was found
 	 */
-	public Line getNextDepartureTimePerLine(int lineNumber, double latitude, double longitude, String clientTimeString, int timeIntervalInMinutes) throws EmptyResultException;
-	
+	public Line getNextDepartureTimePerLine(int lineNumber, double latitude,
+			double longitude, String clientTimeString, int timeIntervalInMinutes)
+			throws EmptyResultException;
 
 	/**
-	 * 
-	 * Find the closest stop to the given latitude/longitude coordination (using "Haversine" formula).
+	 * Find the closest stop to the given latitude/longitude coordination (using
+	 * "Haversine" formula).
 	 * 
 	 * 
 	 * @param latitude
@@ -37,40 +41,85 @@ public interface IDBServices {
 	 * @param longitude
 	 *            the longitude value of the bus station
 	 * @param isRecursive
-	 *            when set to true, search will double the radius in each iteration until a station will be found (default: false) 
+	 *            when set to true, search will double the radius in each
+	 *            iteration until a station will be found (default: false)
 	 * @param maxNumOfIterations
-	 *            maximum number of iterations when the search is recursive (each iteration translates to a query)            
+	 *            maximum number of iterations when the search is recursive
+	 *            (each iteration translates to a query)
 	 * 
 	 * @return bus stop containing the relevant data
-	 * @throws EmptyResultException thrown when no result was found
+	 * @throws EmptyResultException
+	 *             thrown when no result was found
 	 */
-	public Stop getNearestStop(double latitude, double longitude, boolean isRecursive, int maxNumOfIterations) throws EmptyResultException;
-	
-	
+	public Stop getNearestStop(double latitude, double longitude,
+			boolean isRecursive, int maxNumOfIterations)
+			throws EmptyResultException;
+
 	/**
+	 * Getting the details of a specific trip (basically, what are the stations
+	 * on the rest of the trip)
 	 * 
 	 * @param tripId
 	 *            the id of the chosen trip
 	 * @param currentStopSequenceNumber
 	 *            the stop sequence number of the user current location
 	 * @return return all the stations which comprises this trip continuation
-	 * @throws EmptyResultException thrown when no result was found
+	 * @throws EmptyResultException
+	 *             thrown when no result was found
 	 */
-	public List<Stop> getRouteDetails(long tripID,
-			int currentStopSequenceNumber) throws EmptyResultException;
-	
-	public void increaseUserPoints(long userId,int numOfPints);
-	
-	public void updateCurrentLocationReport (long tripId, double longitude, double latitude, long reporterId);
-	
-	public void clearReport(long tripId);
-	
-	
-	
-	
-	
-	
-	
-	
+	public List<Stop> getRouteDetails(long tripID, int currentStopSequenceNumber)
+			throws EmptyResultException;
+
+	/**
+	 * Increase user's points 
+	 * 
+	 * @param userId
+	 *            the user's id
+	 * @param numOfPoints
+	 *            number of points we want to add for this user
+	 * @return current number of points of this user (after the raise)
+	 * 
+	 */
+	public long increaseUserPoints(long userId, int numOfPoints);
+
+	/**
+	 * Reporting current location of a user on a specific trip.
+	 * 
+	 * @param userId
+	 *            the user's id
+	 * @param logitude
+	 *            current longitude coordinate
+	 * @param latitude
+	 *            current latitude coordinate
+	 * @param tripId
+	 *            the trip's id
+	 */
+	public void reportCurrentLocation(long userId, double logitude,
+			double latitude, long tripId);
+
+	/**
+	 * Report a checkout of a user. This call results in clearing all the user's
+	 * reports on this trip
+	 * 
+	 * @param userId
+	 *            the user's id
+	 * @param tripId
+	 *            the trip's id
+	 */
+	public void reportCheckout(long userId, long tripId);
+
+	/**
+	 * Enter a report message for this specific trip
+	 * 
+	 * @param userId
+	 *            the user's id
+	 * @param tripId
+	 *            the trip's id
+	 * @param reportMessage
+	 *            free text description for the given trip 
+	 *            (max length: 200 characters)
+	 */
+	public void reportTripDescription(long userId, long tripId,
+			String reportMessage);
 
 }

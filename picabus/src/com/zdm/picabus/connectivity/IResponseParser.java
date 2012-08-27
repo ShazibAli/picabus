20e153db05c8201f8eb3442d0a017fd264d48c0e
dlereya@gmail.com <dlereya@gmail.com@e7f404d5-e9fd-1835-0f93-09655e65c390>
2012-08-27 19:45:50 +0000

    updating Response parser - client

--- picabus/src/com/zdm/picabus/connectivity/IResponseParser.java
+++ picabus/src/com/zdm/picabus/connectivity/IResponseParser.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 
 import com.zdm.picabus.enitities.Line;
 import com.zdm.picabus.enitities.RealtimeLocationReport;
+import com.zdm.picabus.enitities.ReportResult;
 import com.zdm.picabus.enitities.Stop;
 
 public interface IResponseParser {
@@ -15,6 +16,8 @@ public interface IResponseParser {
 	public Collection<Stop> parseGetRouteJsonResponse(JSONObject responseJson);
 
 	public long parseGetScoreResponse(JSONObject json);
+	
+	public ReportResult parseReportResult(JSONObject json);
 
 	public RealtimeLocationReport parseRealtimeLocationResponse(JSONObject json);
 	

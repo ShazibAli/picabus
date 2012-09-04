20b42211473b3a8cd734f2196ad0c70e1790b4b7
dlereya <DLereya@gmail.com>
2012-09-04 18:58:13 +0000

    Changing list adapter for the reports - new design

--- picabus/src/com/zdm/picabus/logic/ReportsListAdapter.java
+++ picabus/src/com/zdm/picabus/logic/ReportsListAdapter.java
@@ -19,12 +19,12 @@ public class ReportsListAdapter extends BaseAdapter {
  
     private Activity activity;
     private ArrayList<HashMap<String, String>> data;
-    private static LayoutInflater inflater=null;
+    private static LayoutInflater inflater = null;
 
  
     public ReportsListAdapter(Activity a, ArrayList<HashMap<String, String>> d) {
         activity = a;
-        data=d;
+        data = d;
         inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         
     }
@@ -42,15 +42,15 @@ public class ReportsListAdapter extends BaseAdapter {
     }
  
     public View getView(int position, View convertView, ViewGroup parent) {
-        View vi=convertView;
+        View vi = convertView;
         if (convertView == null) {
         	vi = inflater.inflate(R.layout.list_row, null);
         }
             
  
         TextView reporterName = (TextView)vi.findViewById(R.id.title); 
-        TextView report = (TextView)vi.findViewById(R.id.artist); 
-        TextView timeOfReport = (TextView)vi.findViewById(R.id.duration); 
+        TextView report = (TextView)vi.findViewById(R.id.report); 
+        TextView timeOfReport = (TextView)vi.findViewById(R.id.report_time); 
         ImageView reporterImage = (ImageView)vi.findViewById(R.id.list_image); 
  
         HashMap<String, String> currentReport = new HashMap<String, String>();
@@ -60,7 +60,6 @@ public class ReportsListAdapter extends BaseAdapter {
         reporterName.setText("Picabus User");
         report.setText(currentReport.get(TripManagerActivity.KEY_REPORT));
         timeOfReport.setText(currentReport.get(TripManagerActivity.KEY_REPORT_TIME));
-      //  imageLoader.DisplayImage(song.get(CustomizedListView.KEY_THUMB_URL), reporterImage);
         
         long reporterId = Long.valueOf(currentReport.get(TripManagerActivity.KEY_REPORTER));
         UserNamePicGetter userNamePicRequest = new UserNamePicGetter(currentReport.get(TripManagerActivity.KEY_REPORTER), reporterName, reporterImage);
